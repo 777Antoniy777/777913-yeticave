763351b4f9b52822ac11fc5f2b619c08aec091a9
Антон <tat7106@gmail.com>
2019-02-14 01:26:42 +0600

    fixed some fails about tables

--- schema.sql
+++ schema.sql
@@ -1,10 +1,13 @@
+-- удаление БД, если она уже была ранее создана
+DROP DATABASE [IF EXISTS] yeticave;
 -- создание БД
-CREATE DATABASE yeticave
+CREATE DATABASE yeticave;
 
 -- создание таблицы Категории
 CREATE TABLE category (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  name CHAR(100) UNIQUE NOT NULL
+  name CHAR(100) UNIQUE NOT NULL,
+  alias CHAR(50) NOT NULL
 );
 
 INSERT INTO categories (name) VALUES ("Доски и лыжи"), ("Крепления"), ("Ботинки"), ("Одежда"), ("Инструменты"), ("Разное");
@@ -14,29 +17,25 @@ CREATE TABLE lots (
   id INT AUTO_INCREMENT PRIMARY KEY,
   category_id INT,
   user_id INT,
-  bet_id INT,
   name CHAR(150) NOT NULL,
   description CHAR(255),
-  date TIMESTAMP NOT NULL DEFAULT,
+  date_finish TIMESTAMP,
   url CHAR(50) NOT NULL,
   price INT NOT NULL,
   step INT NOT NULL
 );
 
-INSERT INTO lots (category_id, winner_id, author_id, name, description, date, url, price, step)
+INSERT INTO lots (category_id, name, description, date, url, price, step)
 VALUES
-  (1, 1, 1, 1, "2014 Rossignol District Snowboard", "2014 Rossignol District Snowboard", "...", "...", 10999, 1000),
-  (1, 1, 1, 1, "2014 Rossignol District Snowboard", "2014 Rossignol District Snowboard", "...", "...", 10999, 1000),
-  (1, 1, 1, 1, "2014 Rossignol District Snowboard", "2014 Rossignol District Snowboard", "...", "...", 10999, 1000);
+  (1, "2014 Rossignol District Snowboard", "2014 Rossignol District Snowboard", "...", "...", 10999, 1000),
+  (2, "2014 Rossignol District Snowboard", "2014 Rossignol District Snowboard", "...", "...", 10999, 1000);
 
 -- создание таблицы Пользователи
 CREATE TABLE users (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  lot_id INT,
-  bet_id INT,
   name CHAR(50) NOT NULL,
-  email CHAR(25) NOT NULL UNIQUE,
-  password CHAR(20) NOT NULL,
+  email CHAR(50) NOT NULL UNIQUE,
+  password CHAR(50) NOT NULL,
   avatar CHAR(20),
   date DATETIME NOT NULL,
   contacts CHAR(50)
@@ -45,6 +44,7 @@ CREATE TABLE users (
 -- создание таблицы Ставки
 CREATE TABLE bets (
   id INT AUTO_INCREMENT PRIMARY KEY,
+  lot_id INT,
   date TIMESTAMP NOT NULL,
   price INT NOT NULL
 );
